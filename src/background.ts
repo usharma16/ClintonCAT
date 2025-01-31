d08194ecea6d491cb2b183887f17a25892aa5898
Wayne Keenan <wayne.keenan@gmail.com>
2025-01-31 07:19:56 +0000

    remove tab opening

--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,5 @@
-const STATE_OPEN_DOMAINS = "open_domains";
 import {Preferences} from './storage'
-import {PagesDB} from "./database";
+import {DomainResults, PagesDB} from "./database";
 
 chrome.runtime.onInstalled.addListener(() => {
   console.log("ClintonCAT Extension Installed");
@@ -35,30 +34,6 @@ function extractMainDomain(hostname: string) {
 }
 
 
-
-
-function openBackgroundTab(url: string) {
-  chrome.tabs.create({ url: url, active: false }, (tab) => {
-  });
-}
-
-function openTabIfNotExists(url: string) {
-  // Query all tabs to find if the URL is already open
-  chrome.tabs.query({}, (tabs) => {
-    const existingTab = tabs.find((tab) => tab.url === url);
-    if (!existingTab) {
-      openBackgroundTab(url);
-    }
-  });
-}
-
-
-function foundCATEntry(url: string) {
-  openTabIfNotExists(url);
-}
-
-
-
 function isDomainExcluded(exclusions : string[], domain : string) :boolean  {
   if (exclusions == null) {
     return false;
@@ -66,23 +41,11 @@ function isDomainExcluded(exclusions : string[], domain : string) :boolean  {
   return exclusions.some((excludedDomain: string) => domain.includes(excludedDomain));
 }
 
-async function getOpenDomains(): Promise<string[]> {
-  return new Promise((resolve) => {
-    chrome.storage.local.get(STATE_OPEN_DOMAINS, (result) => {
-      console.log("Open domains: ", result[STATE_OPEN_DOMAINS] );
-      resolve(result[STATE_OPEN_DOMAINS] || []);
-    });
-  });
+function foundCATPages(pages: DomainResults) {
+  indicateCATEntries(pages.numPages);
+  // TODO indicate: pages.pageUrls[0];
 }
 
-async function saveOpenDomains(domainName: string) :Promise<void> {
-  let openDomains: string[] = await getOpenDomains();
-
-  if (!openDomains.includes(domainName)) {
-    openDomains.push(domainName);
-    chrome.storage.local.set({ [STATE_OPEN_DOMAINS]: openDomains});
-  }
-}
 
 async function indicateCATEntries(num: number) :Promise<void> {
   let badgeText: string = Preferences.isEnabled ? "on" : "off";
@@ -95,7 +58,6 @@ async function indicateCATEntries(num: number) :Promise<void> {
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   (async () => {
-
 
     if (message.badgeText) {
       chrome.action.setBadgeText({ text: message.badgeText });
@@ -120,20 +82,9 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       const mainDomain = extractMainDomain(currentDomain);
       console.log("Main domain: " + mainDomain);
 
-      // Block multiple tab openings due to multiple windows and potentially multiple async 'onMessage' invocations
-      // TODO: need to remove domains when tabs/windows close.
-      //       or somehow add state to the tabs we open and search that instead for supressing dupes
-      const openDomains = await getOpenDomains();
-      if (openDomains.includes(mainDomain)) {
-        return;
-      } else {
-        saveOpenDomains(currentDomain);
-      }
-
       pagesDatabase.getPagesForDomain(mainDomain).then((results) => {
         if (results.numPages > 0) {
-          indicateCATEntries(results.numPages);
-          foundCATEntry(results.pageUrls[0]);
+          foundCATPages(results);
         }
       });
     }
