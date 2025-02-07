1389feebfd7363bf8610d4a53cda16294a8bd376
Neil Murphy <barrymun@tcd.ie>
2025-02-07 12:21:36 +0000

    split the webpack code so that other engines can more easily be supported. account for differences in manifest.json files

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,61 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    mode: 'production',
-    entry: {
-        background: './src/background.ts',
-        content: './src/content.ts',
-        popup: './src/popup.tsx',
-        options: './src/options.tsx',
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].js',
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx)$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader'],
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'public/options.html',
-            filename: 'options.html',
-            chunks: ['options'],
-        }),
-        new HtmlWebpackPlugin({
-            template: 'public/popup.html',
-            filename: 'popup.html',
-            chunks: ['popup'],
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: 'public/manifest.json', to: 'manifest.json' },
-                { from: 'public/icons/clinton16.png', to: 'icon16.png' },
-                { from: 'public/icons/clinton32.png', to: 'icon32.png' },
-                { from: 'public/icons/clinton48.png', to: 'icon48.png' },
-                { from: 'public/icons/clinton128.png', to: 'icon128.png' },
-                { from: 'public/images/alert.png', to: 'alert.png' },
+module.exports = (env, argv) => {
+    return {
+        mode: 'production',
+        entry: {
+            background: './src/background.ts',
+            content: './src/content.ts',
+            popup: './src/popup.tsx',
+            options: './src/options.tsx',
+        },
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: '[name].js',
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(ts|tsx)$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.css$/,
+                    use: [MiniCssExtractPlugin.loader, 'css-loader'],
+                },
             ],
-        }),
-        new MiniCssExtractPlugin(),
-    ],
-    devtool: 'source-map',
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                template: 'public/options.html',
+                filename: 'options.html',
+                chunks: ['options'],
+            }),
+            new HtmlWebpackPlugin({
+                template: 'public/popup.html',
+                filename: 'popup.html',
+                chunks: ['popup'],
+            }),
+            new CopyWebpackPlugin({
+                patterns: [
+                    { from: 'public/icons/clinton16.png', to: 'icon16.png' },
+                    { from: 'public/icons/clinton32.png', to: 'icon32.png' },
+                    { from: 'public/icons/clinton48.png', to: 'icon48.png' },
+                    { from: 'public/icons/clinton128.png', to: 'icon128.png' },
+                    { from: 'public/images/alert.png', to: 'alert.png' },
+                ],
+            }),
+            new MiniCssExtractPlugin(),
+        ],
+        devtool: 'source-map',
+    };
 };
