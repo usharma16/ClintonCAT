1dc8539c9d75fe2f5fb59c6bde2a9f58c22b0389
Neil Murphy <barrymun@tcd.ie>
2025-02-11 11:21:11 +0000

    adding a new command which will force jest to run in coverage mode so that devs can see what areas are lacking test coverage, ignoring the generated coverage dir, adding some new tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,18 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
+import type { Config } from 'jest';
+
+const collectCoverage = process.env.COVERAGE === 'true';
+
+const config: Config = {
+    collectCoverage,
+    collectCoverageFrom: ['./src/**/*.{js,jsx,ts,tsx}'],
+    coveragePathIgnorePatterns: ['./src/utils/types.ts'],
+    coverageProvider: 'v8',
     modulePathIgnorePatterns: ['src/contentscanners/test.ts'],
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1',
     },
+    preset: 'ts-jest',
+    testEnvironment: 'node',
 };
+
+export default config;
